48e316997fe880d2acd86bdd4cf1cd8922c9f1d5
Yehoshua Kopyrin <aetente@gmail.com>
2022-02-20 12:19:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const jsonServer = require('json-server')
+
+const middlewares = jsonServer.defaults()
+let db = require("./generateDB");
+const router = jsonServer.router(db())
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const app = express();
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './')));
+
+app.use('/api', router)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on the port::${port}`);
+});
