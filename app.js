684b8fd1dc1ee78eb90500818211f644dfe6d251
Yehoshua Kopyrin <aetente@gmail.com>
2022-02-21 18:13:16 +0200

    moved build files

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const bodyParser = require("body-parser");
 
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, './src')));
 
 app.use('/api', router)
 
